95826425541d490bd3cad45f015c1c44b962c8f7
Roland Puntaier <roland.puntaier@chello.at>
2018-02-21 21:50:54 +0100

    more rst handling for vim

--- rstdoc/vim_rst.py
+++ rstdoc/vim_rst.py
@@ -4,15 +4,30 @@
 """
 In the ``.vimrc`` do::
 
-    from rstdoc.vim_rst import *
+    import rstdoc.vim_rst
 
-The the vim functions ``ReformatTable``, ``ReflowTable`` and ``ReTitle`` are available.
+Then these vim functions will be defined:
+
+- ``ReformatTable`` from e.g. double space separated format (by cursor pos, not '<,'>)
+- ``ReflowTable`` adapts to new first line (by cursor pos, not '<,'>)
+- ``ReTitle`` fixes the header underlines (by cursor pos, not '<,'>)
+- ``UnderLine`` and ``TitleLine`` for titles and headings
+- ``RstDcxInit``: cd, then it will create a sample tree structure
+- ``RstDcx``: will update link and tag files starting from current dir
+- ``ListTable``: convert grid tables to list-table
+
+A sample configuration in the vimrc::
+
+
 """
 
 import vim
+from pathlib import Path
 
 from vim_bridge import bridged
 from .retable import ReformatTable, ReflowTable, ReTitle, get_bounds
+from .dcx import main as dcx
+from .listtable import gridtable
 
 def get_table_bounds():
     row,col = vim.current.window.cursor
@@ -33,3 +48,49 @@ def reflow_table():
 def re_title():
     row,col = vim.current.window.cursor
     ReTitle(vim.current.buffer,row-1,col-1)
+
+@bridged
+def under_line(chr):
+    #chr='='
+    pos=vim.current.window.cursor[0]-1
+    cline=vim.current.buffer[pos]
+    nl=chr*len(cline)
+    vim.current.buffer[pos:pos+1] = [cline,nl]
+
+@bridged
+def title_line(chr):
+    #chr='='
+    pos=vim.current.window.cursor[0]-1
+    cline=vim.current.buffer[pos]
+    nl=chr*len(cline)
+    vim.current.buffer[pos:pos+1] = [nl,cline,nl]
+
+@bridged
+def rst_dcx_init():
+    dcx(root='.',verbose=False)
+    
+@bridged
+def rst_dcx():
+    dcx(root=None,verbose=False)
+    tags=','.join([str(x.absolute()) for x in Path('.').glob("**/.tags")])
+    vim.eval('''execute("set tags=./.tags,.tags,'''+tags.replace('\\','/')+'")')
+
+@bridged
+def list_table(join):
+    s,e=vim.current.range.start,vim.current.range.end
+    print(s,e)
+    lns = ["xxx"]*10
+    lt = list(gridtable(lns,join))
+    #vim.current.buffer[vim.current.range.start:vim.current.range.end+1] = lt
+
+
++-------+-------+
+| xa    | xb    |
++=======+=======+
+| xa    | xb    |
++-------+-------+
+| xa    | xb    |
++-------+-------+
+| xa    | xb    |
++-------+-------+
+
